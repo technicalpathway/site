08c8ca7a2238aab53be59b24dbb1dbd626504279
Pranav Ramesh <laphatize@protonmail.com>
2025-02-03 15:30:36 -0500

    feat: quick bug fixes related to local deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Resource-Policy',
-            value: 'cross-origin',
-          }
-        ],
-      },
-      {
-        // Special header configuration for WebContainers route
-        source: '/ide/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          }
-        ],
-      }
-    ];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+    headers: async () => {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Cross-Origin-Embedder-Policy',
+              value: 'require-corp',
+            },
+            {
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin',
+            },
+            {
+              key: 'Cross-Origin-Resource-Policy',
+              value: 'cross-origin',
+            }
+          ],
+        },
+        {
+          // Special header configuration for WebContainers route
+          source: '/ide/:path*',
+          headers: [
+            {
+              key: 'Cross-Origin-Embedder-Policy',
+              value: 'require-corp',
+            },
+            {
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin',
+            }
+          ],
+        }
+      ];
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
